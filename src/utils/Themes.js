e11843e36a7d1618a0e6ab8e4c8516a540e669a4
Harry0231 <harryrajput544@gmail.com>
2024-03-24 12:06:32 -0700

    react-portfolio

---
+++ src/utils/Themes.js
@@ -0,0 +1,35 @@
+export const darkTheme = {
+    bg:"#1C1C27",
+    bgLight: "#1C1E27",
+    primary:"#854CE6",
+    text_primary:"#F2F3F4",
+    text_secondary:"#b1b2b3",
+    card:"#171721",
+    card_light: '#191924',
+    button:"#854CE6",
+    white:"#FFFFFF",
+    black:"#000000",
+}
+
+// export const lightTheme = {
+//     bg:"#FFFFFF",
+//     bgLight: "#f0f0f0",
+//     primary:"#be1adb",
+//     text_primary:"#111111",
+//     text_secondary:"#48494a",
+//     card:"#FFFFFF",
+//     button:"#5c5b5b",
+// }
+
+export const lightTheme = {
+    bg:"#FFFFFF",
+    bgLight: "#F2F3F4",
+    primary:"#854CE6",
+    text_primary:"#000000",
+    text_secondary:"#707070",
+    card:"#F2F3F4",
+    card_light: '#FFFFFF',
+    button:"#854CE6",
+    white:"#FFFFFF",
+    black:"#000000",
+}
